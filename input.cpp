14bc553f8ce7280293ad50bbf06a1c507145132f
pijaitdm <69780569+pijaitdm@users.noreply.github.com>
2025-08-01 22:58:52 +0700

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -1,36 +1,44 @@
-//inputan user
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 int userInput(const std::string& pesan){
-  while(true){
-  int user;
-  std::cout << pesan;
-  std::cin >> user;
-    if (std::cin.fail()){
-        std::cin.clear();
-        std::cin.ignore(1000, '\n');
-        std::cout << "\n Inputan tidak Valid! \n";
+    while(true){
+        int user;
+        std::cout << pesan;
+        std::cin >> user;
+        if (std::cin.fail()){
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "\n Inputan tidak Valid! \n";
+        }
+        else {
+            return user;
+        }
     }
-    else {
-    return user;
-    }
-  }
 }
-  
-std::string tambah_bulan(std::string par_bulan) {
-    std::string bulan[] = {
+
+std::string input_bulan() {
+    std::string input_bulan_str;
+    const std::vector<std::string> bulan = {
         "januari", "februari", "maret", "april", "mei", "juni",
         "juli", "agustus", "september", "oktober", "november", "desember"
     };
 
-    for (int i = 0; i < 12; ++i) {
-        if (par_bulan == bulan[i]) {
-            return bulan[i];  // dikembalikan
+    while (true) {
+        std::cout << "Masukan Bulan: ";
+        std::cin >> input_bulan_str;
+
+        // Mengubah input menjadi huruf kecil untuk perbandingan yang tidak case-sensitive
+        std::transform(input_bulan_str.begin(), input_bulan_str.end(), 
+        input_bulan_str.begin(),[](unsigned char c){ return std::tolower(c); });
+
+        // Mencari input di dalam vektor bulan
+        if (std::find(bulan.begin(), bulan.end(), input_bulan_str) != bulan.end()) {
+            return input_bulan_str;
+        } else {
+            std::cout << "Bulan tidak valid. Silakan coba lagi.\n";
         }
     }
-
-    std::cout << "Bulan tidak valid.\n";
-    return "";
 }
-  
