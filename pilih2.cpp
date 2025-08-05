b4a3eef5409671a7460cd9a3d6963a62ba7bda51
pijaitdm <69780569+pijaitdm@users.noreply.github.com>
2025-08-05 21:43:51 +0700

    Update pilih2.cpp

--- pilih2.cpp
+++ pilih2.cpp
@@ -10,35 +10,57 @@ std::string tambah_tahun(){
 std::string folder_tahun = lib() + tahun() ;
 if (!std::filesystem::exists(folder_tahun)&&!std::filesystem::is_directory(folder_tahun)){
   std::filesystem::create_directories(folder_tahun);
-  std::cout << tahun() << " berhasil dibuat \n ";
-}
+  std::cout << tahun() << " berhasil dibuat \n ";}
 else{
-std::cout << "(lanjut input untuk tahun " << tahun();
+std::cout << "Tahun : " << tahun() << std::endl;
 }
 
 return folder_tahun;
 }
-//tambah folder bulan
-void tambah_bulan(){
+
+std::string validasi_bulan(){
 std::string bulan; 
-while(true){
+do{
 bulan = input_bulan();
-std::string buat_folder_bulan = tambah_tahun() +"/"+ bulan; 
+}while(bulan.empty());
+return bulan;
+}
+//tambah folder bulan
+std::string tambah_bulan(){
+std::string buat_folder_bulan; 
+std::string bulan = validasi_bulan();
+buat_folder_bulan = tambah_tahun() +"/"+ bulan;
 if(!std::filesystem::exists(buat_folder_bulan) && !std::filesystem::is_directory(buat_folder_bulan)){
   std::filesystem::create_directories(buat_folder_bulan);
-  std::cout << bulan << " berhasil dibuat \n";
-  break;
+  std::cout << validasi_bulan() << " berhasil dibuat \n";}
+return buat_folder_bulan;
 }
-else { std::cout << ", dan bulan " << bulan << " !!)" << std::endl; break;}
-}
-} 
 
+//tambah tempat
+std::string tambah_tempat(){
+std::string tempat;
+std::string buat_folder_tempat = tambah_bulan()+"/"+tempat;
+tempat = inputan("Tempat : ");
+if(!std::filesystem::exists(buat_folder_tempat)&&!std::filesystem::is_directory(buat_folder_tempat)){
+  std::filesystem::create_directories(buat_folder_tempat);
+  std::cout << tempat << " berhasil di buat \n";}
+return buat_folder_tempat;
+}
 
 void pilihan2(){
   std::cout << " Inputan UE  Visit \n";
-  tambah_bulan();
-  std::string validasi;
+  std::string validasi; std::string tanggal; 
+  std::string folder = tambah_tempat();
 do{
-  validasi = inputan(" lanjut input UE ya/tidak ! : ");
+
+std::string buat_folder_tanggal = folder +"/"+tanggal;
+tanggal = inputan("tanggal : ");
+if(!std::filesystem::exists(buat_folder_tanggal)&&!std::filesystem::is_directory(buat_folder_tanggal)){
+  std::filesystem::create_directories(buat_folder_tanggal);
+  std::cout << tanggal << " berhasil di buat \n";
+  break;
+}
+std::cout << "----------------------------- \n" ;
+validasi = inputan(" lanjut input UE ya/tidak ! : ");
 }while(validasi != "tidak");
 }
