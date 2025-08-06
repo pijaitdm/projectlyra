d6dfeca02fe27722b07838ec51a7364d808616fd
pijaitdm <69780569+pijaitdm@users.noreply.github.com>
2025-08-06 20:09:05 +0700

    Update pilih2.cpp

--- pilih2.cpp
+++ pilih2.cpp
@@ -18,29 +18,27 @@ std::cout << "Tahun : " << tahun() << std::endl;
 return folder_tahun;
 }
 
-std::string validasi_bulan(){
-std::string bulan; 
-do{
-bulan = input_bulan();
-}while(bulan.empty());
-return bulan;
-}
 //tambah folder bulan
 std::string tambah_bulan(){
 std::string buat_folder_bulan; 
-std::string bulan = validasi_bulan();
-buat_folder_bulan = tambah_tahun() +"/"+ bulan;
+std::string bulan;
+std::string tahun = tambah_tahun();
+do{
+bulan = validasi_bulan();
+buat_folder_bulan = tahun +"/"+ bulan;
 if(!std::filesystem::exists(buat_folder_bulan) && !std::filesystem::is_directory(buat_folder_bulan)){
   std::filesystem::create_directories(buat_folder_bulan);
   std::cout << bulan << " berhasil dibuat \n";}
+  }while(bulan.empty());
 return buat_folder_bulan;
 }
 
 //tambah tempat
 std::string tambah_tempat(){
 std::string tempat;
-std::string buat_folder_tempat = tambah_bulan()+"/"+tempat;
+std::string folder_bulan = tambah_bulan();
 tempat = inputan("Tempat : ");
+std::string buat_folder_tempat = folder_bulan +"/"+tempat;
 if(!std::filesystem::exists(buat_folder_tempat)&&!std::filesystem::is_directory(buat_folder_tempat)){
   std::filesystem::create_directories(buat_folder_tempat);
   std::cout << tempat << " berhasil di buat \n";}
@@ -51,16 +49,12 @@ void pilihan2(){
   std::cout << " Inputan UE  Visit \n";
   std::string validasi; std::string tanggal; 
   std::string folder = tambah_tempat();
-do{
-
-std::string buat_folder_tanggal = folder +"/"+tanggal;
-tanggal = inputan("tanggal : ");
+  tanggal = inputan("tanggal : ");
+  std::string buat_folder_tanggal = folder +"/"+tanggal;
+  
 if(!std::filesystem::exists(buat_folder_tanggal)&&!std::filesystem::is_directory(buat_folder_tanggal)){
   std::filesystem::create_directories(buat_folder_tanggal);
   std::cout << tanggal << " berhasil di buat \n";
-  break;
 }
 std::cout << "----------------------------- \n" ;
-validasi = inputan(" lanjut input UE ya/tidak ! : ");
-}while(validasi != "tidak");
 }
