843ff177b831ea0c799f1b54eee1e1807f1cbe2f
pijaitdm <69780569+pijaitdm@users.noreply.github.com>
2025-08-06 22:49:02 +0700

    Update pilih1.cpp

--- pilih1.cpp
+++ pilih1.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
-#include "menu.hpp"
-#include "input.hpp"
 #include <fstream>
 #include <sstream>
 #include <filesystem>
 #include <cstdlib>
+#include "menu.hpp"
+#include "input.hpp"
+#include "folder.hpp"
+#include <ctime> // untuk fungsi waktu
 
-//buat file baru
-
-
-//cek file ada atau tidak
-void c_file(){
-  std::string cek_file = "lib/2025.csv";
-    if(!std::filesystem::exists(cek_file)){
-      std::ofstream file(cek_file);
-      std::cout << cek_file << " file berhasil dibuat \n";
-    }
-    else{ std::cout << "Membuka File "<< cek_file << std::endl;}
+std::string get_tahun_sekarang() {
+    time_t t = time(nullptr);
+    tm* waktu = localtime(&t);
+    int tahun = waktu->tm_year + 1900; // tm_year adalah jumlah tahun sejak 1900
+    return std::to_string(tahun);
 }
 
+void c_folder(){
+  std::string folder = folder_lcr();
+  std::string laporan_lcr = folder +"/"+ get_tahun_sekarang();
+  std::filesystem::create_directory(folder);
+    if(!std::filesystem::exists(folder)){
+      std::ofstream file(laporan_lcr);
+      std::cout << " file berhasil dibuat \n";
+    }
+    else{ std::cout << "Membuka File -- > "<<laporan_lcr << std::endl;}
+}
 
-   
 void pilihan1() {
-c_file();// untuk membuat folder dan file  csv
+c_folder();// untuk membuat folder dan file  csv
 std::string lanjut;
+std::string file_csv = folder_lcr();
 
 do{
-  std::ofstream file("lib/2025.csv", std::ios::app);
+  std::ofstream file(file_csv, std::ios::app);
   std::string moon  = validasi_bulan();
   if(moon.empty()){
     std::cout << " dibatalkan kembali  ke menu \n";
@@ -37,10 +43,8 @@ do{
   std::cout << "simpan dan lanjut tekan [1], kembali ke menu [0,,,] : ";
   std::cin >> lanjut;
   if (lanjut == "1"){
-    file << moon << ",";
-    std::cout << moon << " berhasil ditambahkan \n";
-    file << input_ue << std::endl;
-    std::cout << input_ue << " berhasil ditambahkan \n";
+    file << moon << "," << input_ue << std::endl;
+    std::cout << moon << " = " << input_ue << " (berhasil ditambahkan) \n";
     file.close();
   }
   
