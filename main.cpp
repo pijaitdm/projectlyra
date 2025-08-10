4939cb8e3ae05317a53f841ad2c15982f9d61eb8
pijaitdm <69780569+pijaitdm@users.noreply.github.com>
2025-08-10 23:21:44 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,45 +9,40 @@
 #include "pilih3.hpp"
 
 int main () {
-
-menu();
 int pilihan;
 do{ 
+    menu();
     pilihan = userInput("Pilih menu : ");
     switch(pilihan){
       case 1:
         system("clear");
         std::cout << "menu 1 \n";
         pilihan1();
-        menu();
         break;
         
       case 2:
         system("clear");
         std::cout << "Menu 2 - Input UE Pos \n";
         pilihan2();
-        menu();
         break;
         
       case 3:
         system("clear");
         std::cout << "menu 3 \n";
         pilihan3();
-        menu();
         break;
       
       case 5:
         system("clear");
-        menu();
+        break;  
+      case 0:
+        std::cout << "keluar \n";
         break;
         
       default:
         std::cout << "Pilihan tidak valid.\n";
         break;
-        
-      case 0:
-        std::cout << "keluar \n";
-        break;
+      
     }
     }while( pilihan != 0);
       
