2957f481842c7cfc4a49751db970f09301b5dcb0
pijaitdm <69780569+pijaitdm@users.noreply.github.com>
2025-08-11 01:22:25 +0700

    Update pilih1.cpp

--- pilih1.cpp
+++ pilih1.cpp
@@ -23,14 +23,16 @@ do{
     return;
   }
   int input_ue = userInput("Masukan UE : ");
-  std::cout << "simpan dan lanjut tekan [1], kembali ke menu [0,,,] : ";
+  std::cout << "simpan dan lanjut tekan [1], batal dan kembali ke menu [0,,,] : ";
   std::cin >> lanjut;
   if (lanjut == "1"){
     file << moon << "," << input_ue << std::endl;
-    std::cout << moon << " = " << input_ue << " (berhasil ditambahkan) \n";
+    std::cout << moon << " : " << input_ue << " (berhasil ditambahkan) \n";
   }
   
-  else {std::cout << "kembali ke menu \n";}
+  else {
+  std::cout << moon << " & " << input_ue << " tidak tersimpan \n" << "kembali ke menu \n";
+  }
 }
 while(lanjut != "0");
 file.close();
