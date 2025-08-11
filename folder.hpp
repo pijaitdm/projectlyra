d93bc574d2a63364b5f9acc55da7b6bed317bf49
pijaitdm <69780569+pijaitdm@users.noreply.github.com>
2025-08-11 20:30:58 +0700

    Update folder.hpp

--- folder.hpp
+++ folder.hpp
@@ -1,7 +1,5 @@
 #ifndef FOLDER_HPP
 #define FOLDER_HPP
-#include <vector>
 std::string folder_lcr();
-std::string buat_folder_bertingkat(const std::vector<std::string>& tingkat);
 std::string c_folder();
 #endif
